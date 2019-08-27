ef9a6ad52a6b491b58020d329626b75a614dedaa
Sebastian Dunn <cuboxylate@gmail.com>
2019-08-28 00:34:05 +1200

    3DMD - It looks like it works! :D - Add better initial lattice placement - Lots of tidyup and commenting

--- 3dmd.py
+++ 3dmd.py
@@ -16,9 +16,9 @@ import math
 from datetime import datetime
 import os
 import numpy as np
+import random
 
 
-# the main loop function
 def main(md):
     # Set up output files
     timestampString = datetime.now().strftime('%Y-%m-%d.%H:%M:%S')
@@ -59,77 +59,96 @@ def main(md):
 
 
 class MD(object):
-    N = 15  # number of particles (integer for loop control)
+    N = 100  # number of particles (integer for loop control)
     dN = float(N)  # number of particles (double for doing maths)
-    boxLength = 50.0  # length of 1D box
+    boxLength = 25.0  # length of 1D box
     dim = 3.0  # dimensions
 
-    # initialise positions and velocites
+    """
+    Initialise positions and velocities. 
+    """
     def __init__(self):
 
         # declare the global variables
-        # constants
-        self.a = self.boxLength / self.dN  # lattice spacing
+
+        # Constants
+        self.lattice_step = self.boxLength / self.dN  # lattice spacing
         self.dtsteps = 500.0  # number of time steps (double for math)
         self.tsteps = int(self.dtsteps)  # number of time steps (integer for counting)
         self.dt = 0.01  # integration timestep
         self.rc = self.boxLength / 2  # distance cutoff for computing LJ interactions
         self.rc2 = self.rc ** 2  # distance cutoff squared
         self.ecut = 4.0 * ((self.rc ** -12) - (self.rc ** -6))  # value of LJ potential at r = rc: 4(1/rc^{12} - 1/rc^{6})
-
-        # values that change during the simulation
-        self.en = 0.0  # potential energy
-        self.energy_total = 0.0  # total energy (pot + kin)
-        self.temp = 1.728  # temperature
+        self.starting_temp = 5.728  # temperature to scale lattice energies to initially
 
         # lists for storing stuff. All 3 dimensional for x, y, z
-
         self.coords = np.zeros([self.N, 3])  # coordinates
         self.prev_coords = np.zeros([self.N, 3])  # previous coordinates
-
-        self.initial_velocities = np.zeros([self.N, 3])  # velocities
         self.forces = np.zeros([self.N, 3])  # forces
+        self.initial_velocities = np.zeros([self.N, 3])  # velocities. Not updated as they are calculated fresh each dt
 
-        # store data for averaging
+        # store data for averaging at the end
         self.sum_temps = 0.0
-        self.sum_energy_totals = 0.0
         self.temps = np.zeros([self.tsteps])
-        self.enery_totals = np.zeros([self.tsteps])
+        self.sum_energy_totals = 0.0
+        self.energy_totals = np.zeros([self.tsteps])
 
         print("#---- Initialising positions and velocities ----")
 
         sumv = np.zeros([3])  # sum of velocities
         sumv2 = 0.0  # sum of velocities squared
 
-        # loop over the particles
+        # loop over the particles, giving each position and velocity
         for i in range(self.N):
-            # coords start in a very simple diagonal in 3D space. I want to focus on
-            # getting the calculations working before a 3D random spacing algorithm
-            position = self.lattice_pos(i)
-            self.coords[i] = np.repeat(position, 3)  # place particles on a lattice
+            self.coords[i] = self.rand_lattice_pos()  # place particles on a lattice
 
             # assign velocities uniformly (but randomly) in range [-0.5, 0.5]
             self.initial_velocities[i] = np.random.rand(3) - 0.5
 
             sumv += self.initial_velocities[i]  # sum velocities
-            sumv2 += np.sum(np.square(self.initial_velocities[i]))  # sum squared velocities todo this feels wrong.
+            sumv2 += np.sum(np.square(self.initial_velocities[i]))  # sum squared velocities
 
         sumv = sumv / self.dN  # finish calculating velocity of centre of mass
         sumv2 = sumv2 / self.dN  # mean-squared velocity (3 dims added together)
-        sf = math.sqrt(self.dim * self.temp / sumv2)  # scale factor for velocites to achieve desired temperature
+
+        # scale factor for velocities to achieve desired temperature
+        sf = math.sqrt(self.dim * self.starting_temp / sumv2)
 
         for i in range(0, self.N):
             self.initial_velocities[i] = (self.initial_velocities[i] - sumv) * sf  # scale velocites
             self.prev_coords[i] = self.coords[i] - (self.initial_velocities[i] * self.dt)  # set previous positions
 
-    # place particles on a lattice
-    def lattice_pos(self, i):
-        pos = (i + 0.5) * self.a
+    # sets for initialising lattice positions
+    x_positions = set(range(N))
+    y_positions = set(range(N))
+    z_positions = set(range(N))
+
+    """
+    Returns a random point in the lattice.
+    Will not return the same point twice in a run. 
+    """
+    def rand_lattice_pos(self):
+        # Take a random element from the set of possible positions
+        x_pos = random.sample(self.x_positions, 1)[0]
+        y_pos = random.sample(self.y_positions, 1)[0]
+        z_pos = random.sample(self.z_positions, 1)[0]
+
+        # Remove them from the sets so there's no repetition
+        self.x_positions.remove(x_pos)
+        self.y_positions.remove(y_pos)
+        self.z_positions.remove(z_pos)
+
+        pos = (np.array([x_pos, y_pos, z_pos]) + 0.5) * self.lattice_step
         return pos
 
-    # calculate forces
+    """
+    Calculate forces on each particle using Lennard-Jones
+    interactions.
+    
+    Returns the potential energy of the system
+    """
     def force(self):
-        print("#---- Calculating forces ----")
+        # print("#---- Calculating forces ----")
         en = 0.0  # (re)set energy to zero
 
         # (re)set forces to zero
@@ -138,26 +157,17 @@ class MD(object):
         # loop (inefficiently) over all pairs of atoms
         for i in range(0, self.N - 1):
             for j in range(i + 1, self.N):
-                # absolute distance used for boundary checking and
-                # calculating Lennard-Jones potential
-                # xr = self.distance_from(self.coords[i], self.coords[j])
-
                 # get the closest image of coord j based on box conditions
-                adjusted_j = self.closest_j(i, j)
-
-                # periodic boundary conditions
-                # boundary_adjustment = self.boxLength * round(xr / self.boxLength)
-                # xr -= boundary_adjustment
+                adjusted_j = self.find_closest_particle(i, j)
 
                 r2 = self.dist_squared(self.coords[i], adjusted_j)  # square to compare to cutoff
                 if r2 < self.rc2:  # test cutoff
-                    # compute Lennard-Jones interaction
+                    # compute Lennard-Jones interaction from absolute distance
                     r2i = 1.0 / r2
                     r6i = r2i ** 3
                     ff = 48.0 * r2i * r6i * (r6i - 0.5)
 
-                    # update forces
-                    # adjusted_j = self.coords[j] - boundary_adjustment
+                    # update forces in each dimension
                     self.forces[i] += ff * (self.coords[i] - adjusted_j)
                     self.forces[j] -= ff * (self.coords[i] - adjusted_j)
 
@@ -165,14 +175,17 @@ class MD(object):
                     en += 4.0 * r6i * (r6i - 1.0) - self.ecut
         return en
 
-    # finds the closest image of particle j to particle i, based on PBC
-    # This is based on the idea that in a periodic box, an image of a particle
-    # repeats every boxLength in each dimension. Se we do a quick search
-    # of them all and return the point that is closest
-    def closest_j(self, i, j):
+    """
+    Finds the closest image of particle j to particle i.
+    
+    Checks the image 1 boxLength away in each axis, in each
+    combination, returning the one with minimum distance from i
+    """
+    def find_closest_particle(self, i, j):
         coord_i = self.coords[i]
         coord_j = self.coords[j]
 
+        # compare distance squared so we don't have to do expensive sqrt calcs
         min_dist2 = self.dist_squared(coord_i, coord_j)
         closest = coord_j
         for i in [-1, 0, 1]:
@@ -185,41 +198,53 @@ class MD(object):
                         closest = adjusted_j
         return closest
 
-    def dist_squared(self, a, b):
+    @staticmethod
+    def dist_squared(a, b):
         x_term = (a[0] - b[0]) ** 2
         y_term = (a[1] - b[1]) ** 2
         z_term = (a[2] - b[2]) ** 2
         return x_term + y_term + z_term
 
-    def distance_from(self, a, b):
-        return math.sqrt(self.dist_squared(a, b))
-
-    # integrate equations of motion
+    """
+    Integrate the equations of motion.
+    
+        - Use Verlet algorithm to calculate new coordinates for each particle
+        - Calculate and store the instantaneous temperature and potential
+          energy of the system
+          
+    """
     def integrate(self, t, en):
-        print("#---- Integrating equations of motion ----")
+        # print("#---- Integrating equations of motion ----")
 
         sumv = np.zeros(3)
         sumv2 = 0.0
 
         for i in range(0, self.N):
-            new_coords = (2.0 * self.coords[i]) - self.prev_coords[i] + self.dt * self.dt * self.forces[i]  # Verlet algorithm
-            velocity = (new_coords - self.prev_coords[i]) / (2.0 * self.dt)  # velocities in each dim
+            # Verlet algorithm to calculate new coordinates from forces
+            new_coords = (2.0 * self.coords[i]) - self.prev_coords[i] + self.dt * self.dt * self.forces[i]
+
+            # derive the velocity from it to calculate temperature and energy
+            velocity = (new_coords - self.prev_coords[i]) / (2.0 * self.dt)
             sumv += velocity  # velocity centre of mass
             sumv2 += np.sum(np.square(velocity))  # total kinetic energy
-            self.prev_coords[i] = self.coords[i]  # update previous positions
-            self.coords[i] = new_coords  # update current positions
 
-        # calc temp and store to calc standard dev later
-        self.temp = sumv2 / (self.dim * self.dN)  # instantaneous temperature
-        self.sum_temps += self.temp
-        self.temps[t] = self.temp
+            # Update coords for next timestep
+            self.prev_coords[i] = self.coords[i]
+            self.coords[i] = new_coords
 
-        # calc energy and store to calc SD later
-        self.energy_total = (en + 0.5 * sumv2) / self.dN  # total energy per particle
-        self.sum_energy_totals += self.energy_total
-        self.enery_totals[t] = self.energy_total
+        # calculate temp and store to calculate standard dev later
+        temp = sumv2 / (self.dim * self.dN)  # instantaneous temperature
+        self.sum_temps += temp
+        self.temps[t] = temp
 
-    # print coordinates
+        # calculate energy and store to calculate standard dev later
+        energy_total = (en + 0.5 * sumv2) / self.dN  # total energy per particle
+        self.sum_energy_totals += energy_total
+        self.energy_totals[t] = energy_total
+
+    """
+    Print coordinates to file
+    """
     def printcoords(self, time, coordfile):
         coordfile.write('%d\n' % self.N)
         coordfile.write('time %10.10f\n' % time)
@@ -227,7 +252,9 @@ class MD(object):
             adjustedCoord = self.adjust_for_box(self.coords[i])
             coordfile.write('C %-8.8f %-8.8f %-8.8f\n' % (adjustedCoord[0], adjustedCoord[1], adjustedCoord[2]))
 
-    # shift a coordinate to be between 0 and boxLength
+    """
+    Shift a coordinate to be in the box between 0 and boxLength in each axis
+    """
     def adjust_for_box(self, coord):
         adj_coord = np.zeros(3)
 
@@ -240,7 +267,10 @@ class MD(object):
 
         return adj_coord
 
-    # calculate averages, etc and print to file
+    """
+    Calculate average and standard deviation of temperature
+    and potential energy, and print them to file.
+    """
     def statistics(self, tfile, efile):
         # averages
         aveTemp = self.sum_temps / self.dtsteps
@@ -251,7 +281,7 @@ class MD(object):
         varEtot = 0.0
         for i in range(0, self.tsteps):
             varTemp += (self.temps[i] - aveTemp) ** 2
-            varEtot += (self.enery_totals[i] - aveEtot) ** 2
+            varEtot += (self.energy_totals[i] - aveEtot) ** 2
 
         sdTemp = math.sqrt(varTemp / self.dtsteps)
         sdEtot = math.sqrt(varEtot / self.dtsteps)
@@ -259,51 +289,6 @@ class MD(object):
         tfile.write('# Standard deviation: %10.2f\n' % sdTemp)
         efile.write('# Average total energy: %10.2f\n' % aveEtot)
         efile.write('# Standard deviation: %10.2f\n' % sdEtot)
-
-
-
-
-# class coordinate:
-#     def __init__(self, x, y, z):
-#         self.x = x
-#         self.y = y
-#         self.z = z
-#
-#     def plus(self, them):
-#         return coordinate(self.x + them.x, self.y + them.y, self.z + them.z)
-#
-#     def move_by(self, velocities, dt):
-#         return coordinate(self.x + velocities.xv * dt,
-#                           self.y + velocities.yv * dt,
-#                           self.z + velocities.zv * dt)
-#
-#     def scale(self, sf):
-#         return coordinate(self.x * sf, self.y * sf, self.z * sf)
-#
-#
-# class velocities:
-#     def __init__(self, xv, yv, zv):
-#         self.xv = xv
-#         self.yv = yv
-#         self.zv = zv
-#
-#     def plus(self, them):
-#         return velocities(self.xv + them.xv, self.yv + them.yv, self.zv + them.zv)
-#
-#     def minus(self, them):
-#         return velocities(self.xv - them.xv, self.yv - them.yv, self.zv - them.zv)
-#
-#     def scale(self, sf):
-#         return velocities(self.xv * sf, self.yv * sf, self.zv * sf)
-#
-#     def sumv2(self):
-#         return self.xv ** 2 + self.yv ** 2 + self.zv ** 2
-#
-# class forces:
-#     def __init__(self, xf, yf, zf):
-#         self.xf = xf
-#         self.yf = yf
-#         self.zf = zf
 
 
 md = MD()
